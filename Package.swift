77572c3aac6475685526be38c8b0e4339ff634b2
Ryan Gonzalez <rymg19@gmail.com>
2018-04-29 17:40:13 -0500

    Add SwiftPM 4 products

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
-import PackageDescription 
+import PackageDescription
 
 let package = Package(name: "CGLib",
 	pkgConfig: "glib-2.0 gio-unix-2.0",
 	providers: [
 		.Brew("glib glib-networking gobject-introspection"),
 		.Apt("libglib2.0-dev glib-networking gobject-introspection libgirepository1.0-dev")
-	]
+	],
+    products: [
+        .library(name: "CGLib", targets: ["CGLib"]),
+    ],
+    targets: [
+        .target(name: "CGLib"),
+    ]
 )
